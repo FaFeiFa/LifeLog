75716004db46438ab6212ddb8ac96a54fb2dd1f6
sgh <sghlive@sgh.com>
2023-03-24 10:39:20 +0800

    sgh 01 commit

--- src/main/java/com/hua/project/project_first/pojo/ReMsg.java
+++ src/main/java/com/hua/project/project_first/pojo/ReMsg.java
@@ -1,14 +1,16 @@
 package com.hua.project.project_first.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
 import java.util.Map;
 
 @Data
 @ToString
+@AllArgsConstructor
 public class ReMsg {
-    private Boolean status;
-    private Exception error = null;
-    private Map msgMap = null;
+    private Code code;
+    private String error;
+    private Map msgMap;
 }
 
